c43603ca0fd45fa3b88984d44a9c3da92cede158
sJobayer <quyummd@gmail.com>
2020-01-27 00:36:23 -0500

    first commit:Boa test

--- BankOfAmerica/src/main/java/homepage/BoaHomePage.java
+++ BankOfAmerica/src/main/java/homepage/BoaHomePage.java
@@ -1,4 +1,18 @@
 package homepage;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
 public class BoaHomePage {
+
+    @FindBy(how = How.ID, using = "NAV_PERSONAL")
+    public static WebElement personalWebElement;
+
+    @FindBy(how = How.ID, using = "NAV_BUSINESS_ADVANTAGE")
+    public static WebElement smallBussinessWebElement;
+
+    @FindBy(how = How.ID, using = "NAV_WEALTH_MANAGEMENT")
+    public static WebElement wealthManagemetWebElement;
+
 }
